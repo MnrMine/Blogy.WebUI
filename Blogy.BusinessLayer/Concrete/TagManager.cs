88ae4940ef6f340e3323b8ef433bbed551161c48
MnrMine <mnr_mine_agri@hotmail.com>
2024-03-25 01:41:57 +0300

    BusinessLayer-Concrete-...Manager klasörü tamamlama

---
+++ Blogy.BusinessLayer/Concrete/TagManager.cs
@@ -0,0 +1,47 @@
+﻿using Blogy.BusinessLayer.Abstract;
+using Blogy.DataAccessLayer.Abstract;
+using Blogy.DataAccessLayer.EntityFramework;
+using Blogy.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogy.BusinessLayer.Concrete
+{
+    public class TagManager : ITagService
+    {
+        private readonly ITagDal _tagDal;
+
+        public TagManager(ITagDal tagDal)
+        {
+            _tagDal = tagDal;
+        }
+
+        public void TDelete(int id)
+        {
+            _tagDal.Delete(id);
+        }
+
+        public Tag TGetById(int id)
+        {
+            return _tagDal.GetById(id);
+        }
+
+        public List<Tag> TGetListAll()
+        {
+            return _tagDal.GetListAll();
+        }
+
+        public void TInsert(Tag entity)
+        {
+            _tagDal.Insert(entity);
+        }
+
+        public void TUpdate(Tag entity)
+        {
+            _tagDal.Update(entity);
+        }
+    }
+}
