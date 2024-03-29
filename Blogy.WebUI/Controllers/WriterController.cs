5d91e717179c933eb3fdf08c6da1cf38dd9994fb
MnrMine <mnr_mine_agri@hotmail.com>
2024-03-29 03:01:18 +0300

    Component ekleme

---
+++ Blogy.WebUI/Controllers/WriterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blogy.WebUI.Controllers
+{
+	public class WriterController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
